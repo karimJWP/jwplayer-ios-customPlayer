c1a1d4dbf504a83ef53a098b9b5f84bfd0825cfa
Karim Mourra <karim@jwplayer.com>
2015-12-23 18:05:35 -0500

    Initial Commit

---
+++ JWCustomPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JWCustomPlayer
+//
+//  Created by Karim Mourra on 12/23/15.
+//  Copyright © 2015 Karim Mourra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
